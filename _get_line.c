6519df84f072d1852a4e9a888a62c9070d4d360c
Ericfranzee <ericfranzee@gmail.com>
2023-04-26 00:06:37 +0100

    customized manpage for our basic shellcustomized manpage for our basic shell

---
+++ _get_line.c
@@ -0,0 +1,48 @@
+
+#include "shell.h"
+
+/**
+ * get_line - stores into malloced buffer the user's cmd into shell
+ * @str: buffer
+ * Return: num of char read
+ */
+size_t get_line(char **str)
+{
+	ssize_t i = 0, size = 0, t = 0, t2 = 0, n = 0;
+	char buff[1024];
+
+	/* read while there's stdin greater than buffsize; -1 to add a '\0' */
+	while (t2 == 0 && (i = read(STDIN_FILENO, buff, 1024 - 1)))
+	{
+		if (i == -1)
+			return (-1);
+
+		buff[i] = '\0';
+		/* terminate buff with \0 to use with _strcat */
+		n = 0;
+		while (buff[n] != '\0')
+		{
+			if (buff[n] == '\n')
+				t2 = 1;
+			n++;
+		}
+
+		/* copy what's read to buff into get_line's buffer */
+		if (t == 0)
+		/* malloc the 1st tim */
+		{
+			i++;
+			*str = malloc(sizeof(char) * i);
+			*str = _strcpy(*str, buff);
+			size = i;
+			t = 1;
+		}
+		else
+		/* _realloc via _strcat with each loop */
+		{
+			size += i;
+			*str = _strcat(*str, buff);
+		}
+	}
+	return (size);
+}
