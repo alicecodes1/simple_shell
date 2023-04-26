623c5b93ddb02db11332464832285cb5b04b29a2
seriki ifeoluwa <ifeoluwaalice75@gmail.com>
2023-04-26 05:06:44 -0700

    generates and publishes a linked list of environmental variables

--- env_linked_list.c
+++ env_linked_list.c
@@ -1,33 +1,34 @@
 #include "shell.h"
 
 /**
- * env_linked_list - To create a linked list from env var
- * @env: environ var
- * Return: link list
+ * env_linked_list - creates a linked list from environmental variables
+ * @env: environmental variables
+ * Return: linked list
  */
 list_t *env_linked_list(char **env)
 {
 	list_t *head;
-	int h = 0;
+	int i = 0;
 
 	head = NULL;
-	while (env[j] != NULL)
+	while (env[i] != NULL)
 	{
-		add_end_node(&head, env[j]);
-		h++;
+		add_end_node(&head, env[i]);
+		i++;
 	}
 	return (head);
 }
 
 /**
- * _env - to print env var
- * @str: user's commnd into shell (i.e. "env")
- * @env: env var
+ * _env - prints the environmental variables
+ * @str: user's command into shell (i.e. "env")
+ * @env: environmental variables
  * Return: 0 on successful
  */
 int _env(char **str, list_t *env)
 {
-	free_double_ptr(str); /* frees usr input */
-	print_list(env);
+	free_double_ptr(str);
+	print_list(env); /* prints env */
 	return (0);
 }
+
