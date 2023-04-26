362776d1d2a2f748e988e6e2a57e40e7d3cb4527
ericfranzee <ericfranzee@gmail.com>
2023-04-26 14:44:42 -0700

    runs and frees command before exiting program (list error fix)

--- _execve.c
+++ _execve.c
@@ -5,10 +5,10 @@
  * @str: user's typed cmd
  * @env: input the linked ls of envirnment
  */
-void c_exit(char **str, ls_t *env)
+void c_exit(char **str, list_t *env)
 {
 	free_double_ptr(str);
-	free_linked_ls(env);
+	free_linked_list(env);
 	_exit(0);
 }
 
@@ -19,42 +19,42 @@ void c_exit(char **str, ls_t *env)
  * @num: nth user cmd; to be used in erroror msg
  * Return: 0 on success
  */
-int _execve(char **s, ls_t *env, int num)
+int _execve(char **s, list_t *env, int num)
 {
 	char *holder;
-	int stt = 0, t = 0;
+	int status = 0, t = 0;
 	pid_t pid;
 
-	/* check if cmd is absolute path */
+	/* check if command is absolute path */
 	if (access(s[0], F_OK) == 0)
 	{
 		holder = s[0];
 		t = 1;
 	}
+	/* else flesh out full path */
 	else
 		holder = _which(s[0], env);
-	/* if not an exe, free */
+	/* if not an executable, free */
 	if (access(holder, X_OK) != 0)
 	{
 		not_found(s[0], num, env);
 		free_double_ptr(s);
 		return (127);
 	}
-	else /* else fork & execute exe cmd */
+	else /* else fork and execute exe cmd */
 	{
 		pid = fork();
-		if (pid == 0)
+		if (pid == 0) /* if child process, execute */
 		{
 			if (execve(holder, s, NULL) == -1)
 			{
-				not_found(s[0], num, env);
+				not_found(s[0], num, env); /* special err msg */
 				c_exit(s, env);
 			}
 		}
-		else
-		/* if parent, wait for child then free all */
+		else /* if parent, wait for child then free all */
 		{
-			wait(&stt);
+			wait(&status);
 			free_double_ptr(s);
 			if (t == 0)
 				free(holder);
