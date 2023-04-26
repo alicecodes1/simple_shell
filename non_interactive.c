bf1563df2d63637957467d1938460360bd723420
seriki ifeoluwa <ifeoluwaalice75@gmail.com>
2023-04-26 06:59:47 -0700

    handles output when called from outside of shell

--- non_interactive.c
+++ non_interactive.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * c_ignore -  ignores spaces and newlines
+ * c_ignore - custom ignores spaces % newlines
  * (e.g. echo "ls\n ls" | ./a.out)
- * @str: envrion var
- * Return: new str
+ * @str: envrionmental variables
+ * Return: new string
  */
 char *c_ignore(char *str)
 {
@@ -14,18 +14,18 @@ char *c_ignore(char *str)
 }
 
 /**
- * non_interactive - handles when user pipes commds into shell via pipeline
+ * non_interactive - handles when user pipes commands into shell via pipeline
  * (e.g. echo "ls/nls -al/n" | ./a.out)
- * @env: envr var
+ * @env: envrionmental variables
  */
 void non_interactive(list_t *env)
 {
-	size_t k = 0, n = 0;
+	size_t i = 0, n = 0;
 	int command_line_no = 0, exit_stat = 0;
 	char *command = NULL, *n_command = NULL, **n_line, **token;
 
-	k = get_line(&command);
-	if (k == 0)
+	i = get_line(&command);
+	if (i == 0)
 	{
 		free(command);
 		exit(0);
@@ -36,7 +36,7 @@ void non_interactive(list_t *env)
 	if (n_command != NULL)
 		free(n_command);
 	n = 0;
-i	while (n_line[n] != NULL)
+	while (n_line[n] != NULL)
 	{
 		command_line_no++;
 		token = NULL;
@@ -54,3 +54,4 @@ i	while (n_line[n] != NULL)
 	free_linked_list(env);
 	exit(exit_stat);
 }
+
