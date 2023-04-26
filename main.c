61196be9c9144dea2ffd86bf4f1e4c475d4c99ed
seriki ifeoluwa <ifeoluwaalice75@gmail.com>
2023-04-26 03:22:19 -0700

    holds admission to the program

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 /**
  * main - creates a simple shell
  * @ac: argument count
- * @av: argument vectors
- * @env: env variables
+ * @av: argument vec
+ * @env: env var
  * Return: 0 on successful
  */
 int main(int ac, char **av, char **env)
